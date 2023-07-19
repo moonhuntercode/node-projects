f6aa43552edc56901c2324b080cbb138c5f62638
victor-code <vdcloayza@gmail.com>
2023-07-18 23:32:20 -0400

    practica con web socke

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-console.log(path);
 //settings
 app.set("port", process.env.PORT || 3000);
 // static files
 app.use(express.static(path.join(__dirname, "public")));
 // start the server
-app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(`server on http://localhost:${app.get("port")}`);
+});
+
+// websockets
+const SocketIo = require("socket.io");
+const io = SocketIo(server);
+// console.log(path);
+io.on("connection", (socket) => {
+  console.log("new connection", socket.id);
+  socket.on("chat:message", (data) => {
+    io.sockets.emit("chat:message", data);
+  });
+  socket.on("chat:typing", (data) => {
+    socket.broadcast.emit("chat:typing", data);
+  });
 });
